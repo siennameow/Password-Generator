d1075e696a0d99f88383568dd9ec62215aa34288
Hexuan Li <lihexuan1@gmail.com>
2022-03-30 18:01:09 -0700

    add 'userChoice'function to JS

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,3 +12,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function userChoice() {
+  var password_length = prompt("Please enter length of the password");
+  
+  if (password_length === null || password_length< 8 || password_length > 128) {
+    alert("Your password should be 8-128 caracters!");
+ }
+ 
+ else {
+  var uppercase = confirm("Include Uppercase letter?");
+  var lowercase = confirm("Include Uppercase letter?");
+  var numeric = confirm("Include Uppercase letter?");
+  var specialcharacter = confirm("Include Uppercase letter?");
+  
+        if (uppercase + lowercase + numeric + specialcharacter === 0) {
+      alert("Your password should contain at least one character type!");
+        }
+        else {
+        alert("You are good to go!");
+        }
+  }
+
+}
