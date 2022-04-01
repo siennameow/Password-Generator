e00a9a27c54f0079a0355ec0e004b04436d05459
Hexuan Li <lihexuan1@gmail.com>
2022-03-31 17:12:39 -0700

    complete pseudo code

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,39 +2,22 @@
 //   return "Please Try Again!" ;
 // }
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Step 1: Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-// GIVEN I need a new, secure password
+//Step 2: Generate user pick.
 
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-
-//create arrays for all letter source (Should have some better way , will check later)
+//Step 2.1 create arrays for all letter source (Should have some better way , will check later)
 var uppercaseSource = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseSource = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberSource = ["0","1","2","3","4","5","6","7","8","9"];
 var specialSource = [" ","!","#","$","%","&","'","(",'"',")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
 
 
+//Step 2.2 create a function that draw random character from the source array.
+//Step 2.3 create a function that record userpick and put different variable into different placeholder.
 
+//Step 2.4 create function that to generate user prompt/alert/confirm according to user preference.
 function generatePassword() {
   var password_length = prompt("How many characters would you like your password to contain?");
   
@@ -49,16 +32,30 @@ function generatePassword() {
   var special = confirm("Include special characters?");
   
         if (uppercase + lowercase + number + special === 0) {
-      alert("Your password should contain at least one character type!");
+      alert("Your should select at least one character type!");
         }
         else {
         alert("You are good to go!");
         }
+//Step 2.5 : if user complete the pick, then do the function in step2.3
+
+
+
+//Step 2.6 :collect placeholder result and add it to the final password.
+
+
+
   }
 
 }
 
-// Write password to the #password input
+
+
+//Step 3: Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+//Step 4: Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -67,8 +64,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
 
