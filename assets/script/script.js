3883448e699426ac577a6246f4ec48d14abfbe0f
Hexuan Li <lihexuan1@gmail.com>
2022-03-31 20:50:26 -0700

    organize the code

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,29 +1,28 @@
 // GOAL:    generatePassword() {
 //   return "Please Try Again!" ;
 // }
-//Step 3: Assignment Code
+//Step 1: Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-//Step 1: Add event listener to generate button
+//Step 2: Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Step 2: Generate user pick.
+//Step 3: Generate user pick.
 
-//Step 2.1 create arrays for all letter source (Should have some better way , will check later)
+//Step 3.1 create arrays for all letter source (Should have some better way , will check later)
 var uppercaseSource = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseSource = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberSource = ["0","1","2","3","4","5","6","7","8","9"];
 var specialSource = [" ","!","#","$","%","&","'","(",'"',")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
 
 
-//Step 2.2 create a function that draw random character from the source array.
+//Step 3.2 create a function that draw random character from the source array.
 function draw (pool){
   var pick1 =  Math.floor(Math.random() * pool.length);
   return pool[pick1];
 }
 
-//Step 2.3 create a function that record userpick and put different variable into different placeholder.
+//Step 3.3 create a function that record userpick and put different variable into different placeholder.
     //Make sure I got at least 1 character from the user picked array and add together all the user-pick-arrays to a remaim-array to draw the the rest of password. 
     //The character remain for drawing = the password length they choose - how many character type they choose.
 function record(userInput,sourceArray,recordResult, remainPool) {
@@ -33,75 +32,60 @@ function record(userInput,sourceArray,recordResult, remainPool) {
     remainPool.push(...sourceArray)
     }
 }
-//Step 2.4 create function that to generate user prompt/alert/confirm according to user preference.
+//Step 3.4 create function that to generate user prompt/alert/confirm according to user preference.
 function generatePassword() {
   var passwordLength = prompt("How many characters would you like your password to contain?");
   
   if (passwordLength === null || passwordLength< 8 || passwordLength > 128) {
-    alert("Your password should be at least 8 characters and no more than 128 characters!");
- }
+          alert("Your password should be at least 8 characters and no more than 128 characters!");
+      }
  
   else {
-  var uppercase = confirm("Include uppercase characters?");
-  var lowercase = confirm("Include lowercase characters?");
-  var number = confirm("Include numeric characters?");
-  var special = confirm("Include special characters?");
+        var uppercase = confirm("Include uppercase characters?");
+        var lowercase = confirm("Include lowercase characters?");
+        var number = confirm("Include numeric characters?");
+        var special = confirm("Include special characters?");
   
         if (uppercase + lowercase + number + special === 0) {
-      alert("Your should select at least one character type!");
-        }
+            alert("Your should select at least one character type!");
+          }
         else {
-        alert("You are good to go!");
+                alert("You are good to go!");
 
-        //Step 2.5 : if user complete the pick and "Good to go!", then do the function in step2.3
-        var recordResult = [];
-        var remainPool = [];
+            //Step 3.5 : if user complete the pick and "Good to go!", then do the function in step3.3
+                var recordResult = [];
+                var remainPool = [];
 
-        record(uppercase,uppercaseSource,recordResult,remainPool);
-        record(lowercase,lowercaseSource,recordResult,remainPool);
-        record(number,numberSource,recordResult,remainPool);
-        record(special,specialSource,recordResult,remainPool);
+                record(uppercase,uppercaseSource,recordResult,remainPool);
+                record(lowercase,lowercaseSource,recordResult,remainPool);
+                record(number,numberSource,recordResult,remainPool);
+                record(special,specialSource,recordResult,remainPool);
 
-        //Step 2.6 :collect placeholder result and add it to the initial password.  
-        var remainAttempt = passwordLength - (uppercase+lowercase+number+special);
-
-        for (var i = 0; i<remainAttempt; i++) {
-                  var record2Draw = draw(remainPool);
-                  recordResult.push(record2Draw);
-            }
-
-          var finalPassword = recordResult.join("");
-          return finalPassword;
-        }
-
-// Step 2.7 : Shuffle the initial password to generate the final password.
-            // function shuffle(meow) {
-  
-            //       var finalPassword = [];
-            //       var restDraw = meow ;
-            //       for (var i = 0; i<meow.lenth; i++) {
-            //               var pick =  Math.floor(Math.random() * restDraw.length);
-            //               finalPassword.push(restDraw[pick]);
-            //               restDraw.splice(pick, 1);
-            //           }
-                  
-            //       return finalPassword;
-            //     }
-
-            //   var shuffledPassword = shuffle(recordResult);
-
-            //   var finalPassword = shuffledPassword.join("");
-    
-            //   return finalPassword;
-
-
-  }
-
+            //Step 3.6 :collect placeholder result and add it to the initial password.  
+                var remainAttempt = passwordLength - (uppercase+lowercase+number+special);
+                for (var i = 0; i<remainAttempt; i++) {
+                      var record2Draw = draw(remainPool);
+                      recordResult.push(record2Draw);
+                    }
+              // var finalPassword = recordResult.join("");
+              // return finalPassword;
+              // Step 3.7 : Shuffle the initial password to generate the final password.
+                function shuffle(meow) {
+                    var finalPassword = [];
+                    var restDraw = meow ;
+                    for (var i = 0; i<meow.lenth; i++) {
+                            var pick =  Math.floor(Math.random() * restDraw.length);
+                            finalPassword.push(restDraw[pick]);
+                            restDraw.splice(pick, 1);
+                        }
+                    return finalPassword;
+                  }
+                var shuffledPassword = shuffle(recordResult);
+                var finalPassword = shuffledPassword.join("");
+                return finalPassword;
+          }
+       }  
 }
-
-//Step 3: Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 
 //Step 4: Write password to the #password input
 function writePassword() {
@@ -109,7 +93,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 
